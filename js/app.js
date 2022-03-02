b4b69005415ba0f1a8a24f9606f5b211435424bb
trishapaulcse <33014659+trishapaulcse@users.noreply.github.com>
2022-03-02 22:55:59 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -30,7 +30,6 @@ const displayData = phones => {
         <p class="card-text">${phone.phone_name}</p>
         <button class ="bg-primary" onclick="phoneDetails('${phone.slug}')" >Details</button>
         </div>
-        <p class ="card-text text-center">${phone.slug}</p>
         `;
         div.appendChild(phonesDiv);
     })
@@ -39,7 +38,7 @@ const phoneDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => displayphoneDetails = (data.data));
+        .then(data => displayphoneDetails(data.data));
 }
 
 const displayphoneDetails = (phoneDetail) => {
@@ -55,8 +54,8 @@ const displayphoneDetails = (phoneDetail) => {
     <div class = "card h-100 mx-auto">
     <img class "w-25 mx-auto mt-2" src="${phoneDetail.image}" class="card-img-top" alt='
     <div class ="card-body text-center">
-    <p class ="card-text"> <b>ChipSet</b>: ${phoneDetail.mainFeatures.chipSet}</p>
-    <p class ="card-text"> <b>DisplaySize:</b> ${phoneDetail.mainFeatures.displaySize}
+    <p class ="card-text"> <b>ChipSet:</b> ${phoneDetail.mainFeatures.chipSet}</p>
+    <p class ="card-text"> <b>DisplaySize:</b> ${phoneDetail.mainFeatures.displaySize}</p>
     <p class="card-text"<b>Memory:</b> ${phoneDetail.mainFeatures.memory}</p>
     <p class="card-text"> <b>Storage:</b> ${phoneDetail.mainFeatures.storage}</p>
     <p class="card-text"> <b>ReleaseDate:</b> ${phoneDetail.releaseDate} ||| 'No release data' </p>
