bb2eeeabffab38eb8da4d9faf9952d2039aecd91
trishapaulcse <33014659+trishapaulcse@users.noreply.github.com>
2022-03-02 14:19:55 +0600

    html added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-const searchPhone = () => {
-    const searchField = document.getElementById('search-field');
+document.getElementById('error-message').style.display = 'none';
+const loadsearchBox = () => {
+    const searchBox = document.getElementById('input-field');
     const searchText = searchBox.value;
     //console.log(searchText);
     searchBox.value = '';
@@ -19,8 +20,15 @@ const displayData = phones => {
     div.innerHTML = '';
     phones.forEach(phone => {
         div.classList.add('col');
-        const phoneDiv = document.createElement('div');
+        const phonesDiv = document.createElement('div');
         phonesDiv.innerHTML = `
         <div class ="card h-100">
+        <img class ="w-50 mx-auto mt-2" src ="${phone.image}" class ="card-img-top" alt="...">
+        <div class ="card-body">
+        <h5 class ="card-text">${phone.brand}</h5>
+        <p class="card-text">${phone.phone_name}</p>
+        <button class ="bg-primary" onclick="phoneDetails('${phone.slug})" >Details</button>
+        </div>
+        <p class ="card-text text-center">${phone.slug}</p>
     })
 }
